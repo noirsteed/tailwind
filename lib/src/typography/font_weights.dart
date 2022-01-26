eef5cc935c4b8e1509ca86dca794fa22088c12d7
Noirsteed <noirsteed@protonmail.com>
2022-01-27 01:40:51 +1000

    Add font weight utilities

---
+++ lib/src/typography/font_weights.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class FontWeights {
+  static const FontWeight fontThin = FontWeight.w100;
+  static const FontWeight fontExtralight = FontWeight.w200;
+  static const FontWeight fontLight = FontWeight.w300;
+  static const FontWeight fontNormal = FontWeight.w400;
+  static const FontWeight fontMedium = FontWeight.w500;
+  static const FontWeight fontSemibold = FontWeight.w600;
+  static const FontWeight fontBold = FontWeight.w700;
+  static const FontWeight fontExtrabold = FontWeight.w800;
+  static const FontWeight fontBlack = FontWeight.w900;
+}
--- lib/tailwind.dart
+++ lib/tailwind.dart
@@ -3,3 +3,4 @@ library tailwind;
 export 'src/borders/border_radii.dart';
 export 'src/colors.dart';
 export 'src/typography/font_sizes.dart';
+export 'src/typography/font_weights.dart';
